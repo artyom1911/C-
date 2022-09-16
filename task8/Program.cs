4dd180e3977b51e322fd678a5768c8c272456d4b
Artyom Prudov <artyom1911@gmail.com>
2022-09-16 23:05:22 +0300

    Finish  task6

--- task8/Program.cs
+++ task8/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8 */
+
+Console.Write ("Введите число 1 ");
+int number1 = int.Parse(Console.ReadLine());
+int even_number = 2;
+
+while (number1 >= even_number )
+{
+    Console.WriteLine ($"{even_number}");
+    even_number = even_number + 2;
+}
