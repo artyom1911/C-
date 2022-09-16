758b149a9d8fd5cc62a1d498b870c2a0db4e97ec
Artyom Prudov <artyom1911@gmail.com>
2022-09-16 22:41:59 +0300

    Finish task6

--- task6/Program.cs
+++ task6/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+Console.Write ("Введите число 1 ");
+int number1 = int.Parse(Console.ReadLine());
+
+
+if(number1 % 2 == 0)
+    Console.WriteLine ($"{number1} является четным");
+else
+    Console.WriteLine ($"{number1} является нечетным");
+
