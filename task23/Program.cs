51fc777ec7c59fc53eaf142d71b15732d0053104
Artem Prudov <artyom1911@gmail.com>
2022-10-02 22:41:03 +0300

    Finish task23

--- task23/Program.cs
+++ task23/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+int end = Convert.ToInt32(Console.ReadLine());
+
+for (int start = 1; start <= end; start++)
+{
+    Console.WriteLine(start * start* start);
+}
