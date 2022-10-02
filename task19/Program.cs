6033ea0dfba5db9554e0bd672f68a85c7ce89aa9
Artem Prudov <artyom1911@gmail.com>
2022-10-02 12:08:44 +0300

    Finish task19

--- task19/Program.cs
+++ task19/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int check = number1 % 100;
+int check2 = number1 / 1000;
+
+if (check/10 == check2 % 10 && check % 10 == check2 / 10)
+{
+    Console.WriteLine("Число является палиндромом");
+}
+
+else
+{
+    Console.WriteLine("Число не является палиндромом");
+}
