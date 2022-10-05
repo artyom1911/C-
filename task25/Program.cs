4516a1735168d3b927aa0a62f1b6937741e982f1
Artem Prudov <artyom1911@gmail.com>
2022-10-05 21:31:06 +0300

    Finish task25

--- task25/Program.cs
+++ task25/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Cubing(int number1, int number2)
+{
+    int cub = 1;
+
+    for (int i = 0; i < number2; i += 1)
+    {
+        cub = cub * number1;
+    }
+    return cub;
+}
+int res = Cubing(3, 5);
+
+Console.WriteLine(res);
+
+// double res = Math.Pow (2,4);
+// Console.WriteLine(res);
