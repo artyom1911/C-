626ec8efa2b09da99c5fd75714366f8539a1d081
Artem Prudov <artyom1911@gmail.com>
2022-10-05 22:50:01 +0300

    Finish task27

--- task27/Program.cs
+++ task27/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+int dig = 0;
+int sum = 0;
+
+while (number1 > 0)
+{
+    dig = number1 % 10;
+    sum = sum + dig;
+    number1 = number1 / 10;
+}
+
+Console.WriteLine(sum);
