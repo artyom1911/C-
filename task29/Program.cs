6e97d7e1b56e75445cd2756c4c7acd67691dab19
Artem Prudov <artyom1911@gmail.com>
2022-10-06 10:58:01 +0300

    Finish task29

--- task29/Program.cs
+++ task29/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int [] myArr = new int [size];
+
+for (int i=0; i < myArr.Length; i+=1)
+{
+    Console.Write($"Введите значение элимента под индексом {i} = ");
+    myArr[i]= Convert.ToInt32(Console.ReadLine());;
+    
+}
+
+    Console.WriteLine($"Массив из {size} элементов: {string.Join(", ", myArr )} ");
+
