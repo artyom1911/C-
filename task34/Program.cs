948f0a4b10222ec7e328badaee7d3c6f2ce6a7c9
Artyom Prudov <artyom1911@gmail.com>
2022-10-13 21:42:11 +0300

    Finish task34

--- task34/Program.cs
+++ task34/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int [] myArr = new int [size];
+int sum = 0;
+
+for (int i = 0; i < size; i+=1)
+{
+    myArr [i] = new Random().Next (100, 999);
+        if (myArr[i]% 2 == 0)
+        {
+            sum++;
+        }
+}
+
+
+
+ Console.WriteLine($"Массив из {size} элементов: {string.Join(", ", myArr )} , Четных чисел = {sum}  ");
