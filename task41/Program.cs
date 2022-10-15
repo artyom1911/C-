6f4ca0678cf921ceb9ee276a5d589b76d3fab388
Artyom Prudov <artyom1911@gmail.com>
2022-10-16 00:06:31 +0300

    Finish task41

--- task41/Program.cs
+++ task41/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Определите колличество значений ");
+int size = Convert.ToInt32(Console.ReadLine());
+int [] myArr = new int [size];
+int diff = 0;
+
+for (int i = 0; i < size; i+=1 )
+{
+    myArr[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+for (int i = 0; i < size; i+=1 )
+{
+    if (myArr[i] > 0 )
+    {
+        diff+=1;
+    }
+}
+
+Console.WriteLine($"{string.Join(", ", myArr )}  Значений больше 0 = {diff}");
